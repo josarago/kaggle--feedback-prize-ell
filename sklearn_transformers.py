de373e371276d8b79560409d8ba6abd6a1d941b8
Jonathan Saragosti <saragostij+github@gmail.com>
2022-12-01 01:58:36 -0500

    everything but the tokenizer and the model

--- sklearn_transformers.py
+++ sklearn_transformers.py
@@ -92,8 +92,8 @@ class FTLangdetectTransformer(BaseEstimator, TransformerMixin):
 # 		if config.pooling == 'mean':
 # 			self.pool = MeanPooling()
 #
-# 	def fit(self, X, y=None):
-# 		return self
+	# def fit(self, X, y=None):
+	# 	return self
 #
 # 	def prepare_input(self, input_texts):
 # 		"""
@@ -148,105 +148,124 @@ class FTLangdetectTransformer(BaseEstimator, TransformerMixin):
 # 		predictions = np.concatenate(preds)
 # 		return predictions
 #
-# 	def simple_transform(self, series):
-# 		inputs = self.prepare_input(series).to(self.config.inference_device)
-# 		y_preds = self.feature(inputs).to(self.config.output_device)
-# 		return y_preds
-#
-# 	def transform(self, series):
-# 		check_is_fitted(self, ['model', 'tokenizer'])
-# 		if self._batch_inference:
-# 			return self.batch_transform(series)
-# 		else:
-# 			return self.simple_transform(series)
+	# def simple_transform(self, series):
+	# 	inputs = self.prepare_input(series).to(self.config.inference_device)
+	# 	y_preds = self.feature(inputs).to(self.config.output_device)
+	# 	return y_preds
+	#
+	# def transform(self, series):
+	# 	check_is_fitted(self, ['model', 'tokenizer'])
+	# 	if self._batch_inference:
+	# 		return self.batch_transform(series)
+	# 	else:
+	# 		return self.simple_transform(series)
 
-def prepare_input(
-		config,
+
+class PooledDeBertaTransformer(TransformerMixin):
+	def __init__(self, config, batch_inference=True):
+		self.config = config
+		self._batch_inference = batch_inference
+
+	def fit(self, X, y=None):
+		return self
+
+	def prepare_input(
+		self,
 		tokenizer,
 		input_texts
 	):
-	"""
-		this will truncate the longest essays.
-		Consider adding some sliding window logic and pooling/aggregation to capture the full text?
-	"""
-	if isinstance(input_texts, pd.Series):
-		texts_list = input_texts.values.tolist()
-	else:
-		texts_list = input_texts
-	inputs = tokenizer.batch_encode_plus(
-		texts_list,
-		return_tensors=None,
-		add_special_tokens=True,
-		max_length=config.tokenizer_max_length,
-		pad_to_max_length=True,
-		truncation=True
-	)
-	for k, v in inputs.items():
-		inputs[k] = torch.tensor(v, dtype=torch.long)
-	return inputs
+		"""
+			this will truncate the longest essays.
+			Consider adding some sliding window logic and pooling/aggregation to capture the full text?
+		"""
+		if isinstance(input_texts, pd.Series):
+			texts_list = input_texts.values.tolist()
+		else:
+			texts_list = input_texts
+		inputs = tokenizer.batch_encode_plus(
+			texts_list,
+			return_tensors=None,
+			add_special_tokens=True,
+			max_length=self.config.tokenizer_max_length,
+			pad_to_max_length=True,
+			truncation=True
+		)
+		for k, v in inputs.items():
+			inputs[k] = torch.tensor(v, dtype=torch.long)
+		return inputs
+
+	@torch.no_grad()
+	def feature(self, model, config, inputs):
+		model.eval()
+		#         last_hidden_states = self.model(**{k: v.to(self.config._inference_device) for k, v, in inputs.items()})[0]
+		last_hidden_states = model(
+			**{k: v.to(self.config.inference_device) for k, v, in inputs.items()}
+		).last_hidden_state
+		feature = MeanPooling()(
+			last_hidden_states,
+			inputs['attention_mask'].to(self.config.inference_device)
+		)
+		return feature
+
+	def batch_transform(
+			self,
+			tokenizer,
+			model,
+			series
+	):
+		y_preds_list = []
+		data_loader = DataLoader(
+			dataset=series,
+			batch_size=self.config._inference_batch_size,
+			shuffle=False
+		)
+
+		for _series in tqdm(data_loader):
+			_inputs = self.prepare_input(
+				tokenizer,
+				_series
+			).to(self.config.inference_device)
+			with torch.no_grad():
+				__y_preds = self.feature(model, self.config, _inputs)
+			y_preds_list.append(__y_preds.to(self.config.output_device))
+		y_preds = np.concatenate(y_preds_list)
+		return y_preds
+
+	def simple_transform(self, tokenizer, model, series):
+		inputs = self.prepare_input(tokenizer, series).to(self.config.inference_device)
+		y_preds = self.feature(model, self.config, inputs).to(self.config.output_device)
+		return y_preds
+
+	def transform(self, tokenizer, model, series):
+		# check_is_fitted(self, ['model', 'tokenizer'])
+		if self._batch_inference:
+			return self.batch_transform(tokenizer, model, series)
+		else:
+			return self.simple_transform(tokenizer, model, series)
 
 
-@torch.no_grad()
-def feature(model, config, inputs):
-	model.eval()
-	#         last_hidden_states = self.model(**{k: v.to(self.config._inference_device) for k, v, in inputs.items()})[0]
-	last_hidden_states = model(
-		**{k: v.to(config.inference_device) for k, v, in inputs.items()}
-	).last_hidden_state
-	feature = MeanPooling()(
-		last_hidden_states,
-		inputs['attention_mask'].to(config.inference_device)
-	)
-	return feature
-
-# class PooledDeBertaTransformer:
-
-n_samples = 800
+n_samples = 16
 batch_size = 8
 
 if __name__ == "__main__":
-	ftl_transformer = FTLangdetectTransformer(model_path=FASTTEXT_MODEL_PATH)
-	print(ftl_transformer.__class__)
-
-	print(ftl_transformer.fit_transform(
-		pd.Series([
-			"Some text definitely in English",
-			"Un texte absolument pas en Anglais"
-		])
-	))
-	DEVICE = "cpu"
 	deberta_config = MSFTDeBertaV3Config(
 		model_size="base",
 		pooling="mean",
-		inference_device=DEVICE,
+		inference_device="mps",
 		output_device="cpu",
 		inference_batch_size=batch_size
 	)
 
-	# pooled_deberta_transformer = PooledDeBertaTransformer(
-	# 	deberta_config,
-	# 	batch_inference=True
-	# )
-	#
-	# print(pooled_deberta_transformer.simple_transform(
-	# 	pd.Series([
-	# 		"Some text definitely in English",
-	# 		"Another type of text, in English too"
-	# 	])
-	# ).shape)
-
-
-
-	# print(pooled_deberta_transformer.batch_transform(
-	# 	pd.Series([
-	# 		"Some text definitely in English",
-	# 		"Another type of text, in English too"
-	# 	])
-	# ).shape)
 	tokenizer = AutoTokenizer.from_pretrained(deberta_config.tokenizer)
 	model = AutoModel.from_pretrained(deberta_config.model).to(
-			deberta_config.inference_device
-		)
+		deberta_config.inference_device
+	)
+
+	pooled_deberta_transformer = PooledDeBertaTransformer(
+		deberta_config,
+		batch_inference=True
+	)
+
 	series = pd.Series(
 		[
 			"Some text definitely in English.",
@@ -254,27 +273,11 @@ if __name__ == "__main__":
 		] * (n_samples // 2)
 	)
 	print(series.shape)
-	preds = []
-	data_loader = DataLoader(
-		dataset=series,
-		batch_size=deberta_config._inference_batch_size,
-		shuffle=False,
-		# num_workers=4
+	pooled_deberta_transformer.transform(
+		tokenizer,
+		model,
+		series
 	)
-	#         bar = tqdm(enumerate(data_loader), total=len(data_loader))
-	#         for step, inputs in bar:
-	for _series in tqdm(data_loader):
-		_inputs = prepare_input(
-			deberta_config,
-			tokenizer,
-			_series
-		).to(deberta_config.inference_device)
-		with torch.no_grad():
-			y_preds = feature(model, deberta_config, _inputs)
-
-		preds.append(y_preds.to(deberta_config.output_device))
-	predictions = np.concatenate(preds)
-	print(predictions.shape)
 
 
 
